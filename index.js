33e01a77f2a2847653eec240e49b5d7eb4d8231b
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2017-12-22 19:18:41 -0800

    Implement inferring options.name from the filename

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ var replaceExt = require('replace-ext');
 var Transform = require('stream').Transform;
 var tryit = require('tryit');
 var vinylSourcemapsApply = require('vinyl-sourcemaps-apply');
+var path = require('path');
+
+function getName(input) {
+  return path
+    .basename(input)
+    .replace(path.extname(input), '')
+    .replace(/[^a-zA-Z_$0-9]+/g, '_')
+    .replace(/^_/, '')
+    .replace(/_$/, '')
+    .replace(/^(\d)/, '_$1');
+}
 
 module.exports = function gulpSvelte(options) {
   return new Transform({
@@ -35,7 +46,10 @@ module.exports = function gulpSvelte(options) {
       var result;
 
       tryit(function() {
-        result = compile(file.contents.toString(), objectAssign({filename: file.path}, options));
+        result = compile(file.contents.toString(), objectAssign(
+          {filename: file.path, name: file.path && getName(file.path)},
+          options
+        ));
       }, function(err) {
         if (err) {
           if (file.path) {
